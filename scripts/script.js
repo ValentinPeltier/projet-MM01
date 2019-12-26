4e93d678edfc23e02ca5e9fc7b6e2076e425895a
ValentinPeltier <valentinpeltier@yahoo.fr>
2019-12-27 00:41:11 +0100

    improve accessibility, add home page

--- scripts/script.js
+++ scripts/script.js
@@ -22,22 +22,61 @@ const toggleMenu = () => {
     }
 };
 
-window.onload = () => {
+const setVideoContainer = (toggle, state) => {
+	const videoContainer = document.querySelector('#home .video-container');
+
+	if(videoContainer) {
+		if(toggle) {
+			videoContainer.classList.toggle('active');
+		}
+		else if(state) {
+			videoContainer.classList.add('active');
+		}
+		else {
+			videoContainer.classList.remove('active');
+		}
+	}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
 	// Add event listeners
     const audioButton = document.getElementById('audio-button');
     const menuButton = document.getElementById('menu-button');
-
+	const videoButton = document.querySelector('#home .video-button');
+	const videoOverlay = document.querySelector('#home .video-overlay');
+	const videoCloseButton = document.querySelector('#home .video-container .close-button');
+console.log(videoOverlay);
     if(audioButton) {
         audioButton.onclick = toggleAudio;
     }
     if(menuButton) {
         menuButton.onclick = toggleMenu;
-    }
+	}
+	if(videoButton) {
+		videoButton.onclick = () => setVideoContainer(false, true);
+	}
+	if(videoOverlay) {
+		videoOverlay.onclick = () => setVideoContainer(true);
+	}
+	if(videoCloseButton) {
+		videoCloseButton.onclick = () => setVideoContainer(false, false);
+	}
+
 
     // Prevent from clicking on dropdown links in navbar
-    document.querySelectorAll('.nav-links > .dropdown > a[href=""]').forEach((link) => {
+    document.querySelectorAll('.nav-links .dropdown > a[href=""]').forEach((link) => {
         link.addEventListener("click", (event) => {
             event.preventDefault();
         });
 	});
-};
+
+	// Improve accessibility
+	document.querySelectorAll('.nav-links .dropdown-content > a').forEach((link) => {
+        link.addEventListener("focusin", () => {
+            link.parentNode.classList.add('active');
+		});
+		link.addEventListener("focusout", () => {
+            link.parentNode.classList.remove('active');
+        });
+	});
+});
