50681542036eac333c7f55ef569b618d8840a81f
ValentinPeltier <valentinpeltier@yahoo.fr>
2020-01-03 19:14:09 +0100

    quizz now select random questions

--- scripts/quizz.js
+++ scripts/quizz.js
@@ -1,3 +1,5 @@
+const entriesCount = 3; // Choose the number of questions in the quizz
+
 const quizzEntries = [
 	{
 		question: 'Question 1',
@@ -6,7 +8,7 @@ const quizzEntries = [
 			'Réponse 1.2',
 			'Réponse 1.3',
 		],
-		correct: 2,
+		correct: 0,
 	},
 	{
 		question: 'Question 2',
@@ -25,7 +27,7 @@ const quizzEntries = [
 			'Réponse 3.4',
 			'Réponse 3.5',
 		],
-		correct: 0,
+		correct: 2,
 	},
 ];
 
@@ -34,8 +36,10 @@ var quizzState = {
 	answers: [],
 };
 
+var currentEntries = [];
+
 const changePage = (pageShift) => {
-	if(quizzState.page + pageShift < 0 || quizzState.page + pageShift >= quizzEntries.length) {
+	if(quizzState.page + pageShift < 0 || quizzState.page + pageShift >= entriesCount) {
 		return;
 	}
 
@@ -52,16 +56,17 @@ const updatePage = () => {
 	const questionsContainer = document.getElementById('quizz-questions-container');
 
 	// Update footer
-	page.innerText = `Question ${quizzState.page + 1} / ${quizzEntries.length}`;
+	page.innerText = `Question ${quizzState.page + 1} / ${entriesCount}`;
 	buttonPrevious.style.display = quizzState.page === 0 ? 'none' : 'inline-block';
-	buttonNext.style.display = quizzState.page + 1 === quizzEntries.length ? 'none' : 'inline-block';
-	buttonSend.style.display = quizzState.page + 1 === quizzEntries.length ? 'inline-block' : 'none';
+	buttonNext.style.display = quizzState.page + 1 === entriesCount ? 'none' : 'inline-block';
+	buttonSend.style.display = quizzState.page + 1 === entriesCount ? 'inline-block' : 'none';
 
 	// Go to requested question
 	questions.style.left = `-${quizzState.page * 100}%`;
 
 	// Update question container height
-	const questionHeight = document.querySelectorAll('#quizz-questions .question')[quizzState.page].scrollHeight;
+	const entryId = currentEntries[quizzState.page].id;
+	const questionHeight = document.querySelector(`#quizz-questions #question${entryId}`).scrollHeight;
 	questionsContainer.style.height = `${questionHeight}px`;
 };
 
@@ -74,11 +79,11 @@ const checkAnswers = () => {
 	const result = document.getElementById('quizz-result');
 
 	let image = null;
-	let score = quizzEntries.reduce((previous, current, entryIndex) => {
+	let score = currentEntries.reduce((previous, current, entryIndex) => {
 		const answer = quizzState.answers.find((e, i) => i === entryIndex);
 		return previous + (answer === current.correct ? 1 : 0);
 	}, 0);
-	score = Math.round(score / quizzEntries.length * 100);
+	score = Math.round(score / entriesCount * 100);
 
 	if(score > 90) {
 		comment = 'Félicitations !';
@@ -105,8 +110,8 @@ const checkAnswers = () => {
 	result.innerHTML = `Vous avez ${score}% de bonnes réponses.<br />${comment}`;
 
 	// Show correct answers
-	quizzEntries.forEach((entry, i) => {
-		const answer = document.querySelector(`input[name="question${i}"][value="${entry.correct}"] + div + span`);
+	currentEntries.forEach((entry) => {
+		const answer = document.querySelector(`input[name="question${entry.id}"][value="${entry.correct}"] + div + span`);
 		answer.classList.add('correct-answer');
 	});
 
@@ -122,6 +127,7 @@ const resetQuizz = () => {
 	// Reset state
 	quizzState.page = 0;
 	quizzState.answers = [];
+	generateEntries();
 	updatePage();
 
 	// Reset correct answers
@@ -140,23 +146,41 @@ const resetQuizz = () => {
 
 	// Hide result container
 	document.getElementById('quizz-result-container').style.display = null;
+
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-	const buttonPrevious = document.getElementById('quizz-button-previous');
-	const buttonNext = document.getElementById('quizz-button-next');
-	const buttonSend = document.getElementById('quizz-button-send');
-	const buttonReset = document.getElementById('quizz-button-reset');
-	const questions = document.getElementById('quizz-questions');
+const generateEntries = () => {
+	currentEntries = [];
+	const usedIndexes = [];
 
-	quizzEntries.forEach((entry, entryIndex) => {
+	for(let i = 0; i < entriesCount; i++) {
+		// Generate a random integer between 0 and entriesCount (excluded)
+		let randomIndex = null;
+		do {
+			randomIndex = Math.floor(Math.random() * entriesCount);
+		} while(usedIndexes.includes(randomIndex));
+
+		currentEntries.push({
+			id: randomIndex,
+			...quizzEntries[randomIndex],
+		});
+
+		// Store the index to not re-use it
+		usedIndexes.push(randomIndex);
+	}
+
+	// Update DOM
+	const questions = document.getElementById('quizz-questions');
+	questions.innerHTML = '';
+
+	currentEntries.forEach((entry, entryIndex) => {
 		const answers = entry.answers.reduce((previous, current, answerIndex) => (
 			previous + `
 				<label>
 					<input
 						type="radio"
 						value="${answerIndex}"
-						name="question${entryIndex}"
+						name="question${entry.id}"
 						onclick="setAnswer(${entryIndex}, ${answerIndex})"
 					/>
 					<div class="radio-placeholder"></div>
@@ -166,17 +190,25 @@ document.addEventListener('DOMContentLoaded', () => {
 		), '');
 
 		questions.innerHTML += `
-			<div class="question" style="left: ${entryIndex * 100}%">
+			<div id="question${entry.id}" class="question" style="left: ${entryIndex * 100}%">
 				<h2>${entry.question}</h2>
 				${answers}
 			</div>
 		`;
 	});
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	const buttonPrevious = document.getElementById('quizz-button-previous');
+	const buttonNext = document.getElementById('quizz-button-next');
+	const buttonSend = document.getElementById('quizz-button-send');
+	const buttonReset = document.getElementById('quizz-button-reset');
+
+	generateEntries();
+	updatePage();
 
 	buttonPrevious.onclick = () => changePage(-1);
 	buttonNext.onclick = () => changePage(1);
 	buttonSend.onclick = checkAnswers;
 	buttonReset.onclick = resetQuizz;
-
-	updatePage();
 });
